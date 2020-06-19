ed8d4b123ae0dacda2b2800af633ccbe5fae1db1
kalu110 <61694776+kalu110@users.noreply.github.com>
2020-06-19 13:31:45 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function opennav() { 
+document.getElementById('menu').style.display = "block"
+document.getElementById('closeimg').style.display = "block"
+document.getElementById('menuimg').style.display = "none"
+}
+
+
+function closenav() { 
+document.getElementById('menu').style.display = "none"
+document.getElementById('menuimg').style.display = "block"
+document.getElementById('closeimg').style.display = "none"
+}
